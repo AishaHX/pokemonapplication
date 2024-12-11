4e03d6d5f8d9dd6611e9f6b0a0fcf928afb7e49b
AishaHX <ayeshggz@gmail.com>
2024-12-11 16:21:43 +0100

    la till error handling för getPokemonLista och getPokemonDetails, implementerat logik för att rendera <pokemon/> med vald pokemons detaljer, fixat om design i css för pokemonContainer och pokemonCard

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,30 +1,18 @@
-/**
- * 
- komponenten ska visa ut följande data för användarens val av Pokemon:
+import "./App.css";
 
-    Namn
-
-    Bild på Pokemon
-
-    Typ/Typer din pokemon har (dvs “types” i API:et)
-
-    Vikt
-
-    Längd} 
-
- */
-
-function Pokemon({ Data }) {
+const Pokemon = ({ Data }) => {
   const { name, sprites, types, weight, height } = Data;
   return (
     <div className="pokemonCard">
       <img src={sprites.front_default} />
+
       <p>Namn: {name}</p>
+
       <p>Type: {types.map((type) => type.type.name)}</p>
       <p>Vikt: {weight}</p>
       <p>Längd: {height}</p>
     </div>
   );
-}
+};
 
 export default Pokemon;
--- src/PokemonApplication.jsx
+++ src/PokemonApplication.jsx
@@ -1,10 +1,3 @@
-/**
- *
- * När <PokemonApplication/> renderas, ska det göras ett anrop mot API:et för att hämta data för samtliga 151 Pokemon, och spara datat i state.
- *  <PokemonApplication> ska ni sedan skapa dropdown-lista med samtliga namn på alla Pokemon som hämtats.
- *  Användaren ska kunna välja en Pokemon i dropdown-listan, och sedan klicka på knappen för att hämta data för sin valda Pokemon.
- */
-
 import { useEffect, useState } from "react";
 import "./App.css";
 import "./Pokemon";
@@ -13,19 +6,21 @@ import Pokemon from "./Pokemon";
 const pokemonApi = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 function PokemonApplication() {
-  //state för spara listan
+  //state för att spara listan
   const [pokemonLista, setPokemonLista] = useState([]);
-  //state för dropdown listan
+  //state för att spara den pokemon som användaren har valt
   const [selectedPokemon, setSelectedPokemon] = useState("");
-  // state för hämta en specifik pokemons data
+  // state för spara en specifik pokemons detaljer
   const [detailsData, SetDetailsdata] = useState();
 
   useEffect(() => {
     const getPokemonLista = async () => {
       const response = await fetch(pokemonApi);
+      if (!response.ok) {
+        throw new Error("Misslyckades att hämta pokemon data");
+      }
       const pokemonData = await response.json();
       setPokemonLista(pokemonData.results);
-      //fixa error handling här oxå
 
       //console.log(pokemonData.results[0].name);
       /*pokemonData.results.forEach((pokemon) => {
@@ -36,20 +31,21 @@ function PokemonApplication() {
     getPokemonLista();
   }, []);
 
-  // funktion för att hämta den valda pokemons information
+  // funktion för att hämta den valda Pokemon detaljer
 
-  const detailsApi = "https://pokeapi.co/api/v2/pokemon/${selectedPokemon}";
+  const detailsApi = `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`;
 
   const getPokemonDetails = async () => {
     const response = await fetch(detailsApi);
+    if (!response.ok) {
+      throw new Error("Misslyckades att hämta pokemon data");
+    }
     const details = await response.json();
-    //fixa error handling här
-
-    SetDetailsdata(details.results);
+    SetDetailsdata(details);
   };
 
   return (
-    <div className="listaRendering">
+    <div className="pokemonContainer">
       <select
         id=""
         onChange={(e) => setSelectedPokemon(e.target.value)}
@@ -62,8 +58,10 @@ function PokemonApplication() {
           </option>
         ))}
       </select>
-      <button onClick={getPokemonDetails}>Show pokemon Details </button>
-      // funkar inte riktigt här
+      <button onClick={getPokemonDetails} className="show">
+        Show{" "}
+      </button>
+
       {detailsData && <Pokemon Data={detailsData} />}
     </div>
   );
